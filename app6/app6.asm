4ca35940e348608822ca681f64ff2909e681ece9
Muhammed Raşit Eşdemir <mrasitesdemir7@gmail.com>
2022-11-06 16:27:44 +0300

    Add files via upload

---
+++ app6/app6.asm
@@ -0,0 +1,14 @@
+.model small
+.data
+.code
+.startup
+
+MOV AX,44AAH
+MOV BL,AH
+MOV BH,AL
+
+.exit 
+
+;BH = ?
+;BL = ?
+;BX = ?
---
+++ app7/app7.asm
@@ -0,0 +1,11 @@
+.model small
+.data 
+dizi dB 1,28,93,40,5,6,7,255
+.code
+.startup
+
+
+.exit 
+      
+;dizi dB 1,28,93,40,5,6,7,256
+;Error = over 8 bits : 256    
